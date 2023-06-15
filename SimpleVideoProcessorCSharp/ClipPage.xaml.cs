f2596c7dd5f06e78e70b56298f9147b032800c57
Little-He-Guan <53397751+Little-He-Guan@users.noreply.github.com>
2023-06-15 21:13:52 +0800

    Start prototyping UI

--- SimpleVideoProcessorCSharp/ClipPage.xaml.cs
+++ SimpleVideoProcessorCSharp/ClipPage.xaml.cs
@@ -1,12 +1,15 @@
 // Copyright (c) Microsoft Corporation and Contributors.
 // Licensed under the MIT License.
 
+#define ISPROTOTYPING
+
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
@@ -30,20 +33,31 @@ namespace SimpleVideoProcessorCSharp
     {
         public ClipPage()
         {
+            videoInfo = new VideoInfo();
             this.InitializeComponent();
         }
 
+        #region Button Clicks
         /// <summary>
         /// Pick up a video file and sets filePath and isVideoOpened on success
         /// </summary>
         /// <returns></returns>
+#if ISPROTOTYPING
+        private void OpenVideoButton_Click(object sender, RoutedEventArgs e)
+#else
         private async void OpenVideoButton_Click(object sender, RoutedEventArgs e)
+#endif
         {
-            // Clear previous returned file name, if it exists.
-            if(!isVideoOpened)
+            if (!videoInfo.isVideoOpened)
             {
-                filePath = "";
+                // Clear previous info, if it exists.
+                videoInfo.Clear();
 
+#if ISPROTOTYPING
+                // fake video info
+                videoInfo = VideoInfo.getFakeVideoInfo();
+#else
+                // open from a real file
                 {
                     var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
                     // Retrieve the window handle (HWND) of the current WinUI 3 window.
@@ -53,7 +67,7 @@ namespace SimpleVideoProcessorCSharp
                     // Initialize the file picker with the window handle (HWND).
                     WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
 
-                    // Set options for your file picker
+                    // Set options for the file picker
                     openPicker.ViewMode = PickerViewMode.Thumbnail;
                     openPicker.SuggestedStartLocation = PickerLocationId.VideosLibrary;
                     openPicker.FileTypeFilter.Add(".mp4");
@@ -66,53 +80,129 @@ namespace SimpleVideoProcessorCSharp
                     var file = await openPicker.PickSingleFileAsync();
                     if (file != null)
                     {
-                        isVideoOpened = true;
-                        filePath = file.Path;
+                        // Update video info.
+                        // TODO: Call functions from the internal library to complete the info.
+                        videoInfo.isVideoOpened = true;
+                        videoInfo.filePath = file.Path;
                     }
                     else
                     {
+                        // TODO: Show an error message somewher.
                     }
                 }
+#endif
 
                 Update();
             }
         }
 
+        private void ClearVideoButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (videoInfo.isVideoOpened)
+            {
+                videoInfo.Clear();
+
+                Update();
+            }
+        }
+
+        private void StartClippingButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+#endregion
+
         /// <summary>
-        /// Update the visual appearances
+        /// Update the UI elements based on the currently opened video
         /// </summary>
         private void Update()
         {
             // enable or disable elements based on if a video is opened.
-            if(isVideoOpened)
+            if(videoInfo.isVideoOpened)
             {
+                // enable disabled elements
                 ClipTimeExpander.IsEnabled = true;
 
-                // also set the texts
-                VideoNameTextBlock.Text = filePath;
+                // set the texts
+                VideoNameTextBlock.Text = videoInfo.filePath;
                 OpenVideoEntry.Description = "A video is opened. Expand to view more info.";
+
+                // update the preview image
+                WriteableBitmap previewImage = new WriteableBitmap(previewWidth, previewHeight);
+                using (Stream st = previewImage.PixelBuffer.AsStream())
+                {
+                    st.Write(videoInfo.previewImgData, 0, videoInfo.previewImgData.Length);
+                }
+                PreviewImage.Source = previewImage;
             }
             else
             {
+                // disable elements
                 ClipTimeExpander.IsEnabled = false;
 
-                // also set the texts of the file
+                // set the texts
                 VideoNameTextBlock.Text = "No video file is opened.";
                 OpenVideoEntry.Description = "Please open a video file to proceed.";
+
+                // update the preview image
+                PreviewImage.Source = null;
             }
         }
 
-        private String filePath = "";
-        private bool isVideoOpened = false;
+        VideoInfo videoInfo;
+        static readonly int previewWidth = 1280;
+        static readonly int previewHeight = 720;
 
-        private void ClearVideoButton_Click(object sender, RoutedEventArgs e)
+        class VideoInfo
         {
-            if(isVideoOpened)
+            /// <summary>
+            /// Clears all data to their default values. After that, the info represents an unopened video.
+            /// </summary>
+            public void Clear()
             {
                 isVideoOpened = false;
                 filePath = "";
+                previewImgData = null;
+                lengthInSec = 0.0f;
+            }
 
-                Update();
+            // The path to the opened video file.
+            // Is "" iff isVideoOpened = false;
+            public String filePath = "";
+            // The pointer to the preview image data, managed by the internal C++ library.
+            // Is null iff isVideoOpened = false; 
+            public byte[] previewImgData = null;
+            // Length of the video, in seconds.
+            // Is 0.0f iff isVideoOpened = false; 
+            public float lengthInSec = 0.0f;
+
+            public bool isVideoOpened = false;
+
+            /// <summary>
+            /// Returns fake info for prototyping.
+            /// </summary>
+            /// <returns>Fake video info</returns>
+            public static VideoInfo getFakeVideoInfo()
+            {
+                VideoInfo ret = new VideoInfo();
+
+                ret.isVideoOpened = true;
+                ret.filePath = "Prototyping\\Somewhere\\somevideo.mp4";
+                ret.lengthInSec = 134.5f;
+                ret.previewImgData = new byte[4 * previewHeight * previewWidth];
+                for(int i = 0; i < previewWidth; ++i)
+                {
+                    for(int j = 0; j < previewHeight; ++j)
+                    {
+                        int colorIndex = 4 * (i * previewHeight + j);
+                        ret.previewImgData[colorIndex] = (byte)(i % 128); // b
+                        ret.previewImgData[colorIndex+1] = (byte)(i % 128); // g
+                        ret.previewImgData[colorIndex+2] = (byte)(i % 128); // r
+                        ret.previewImgData[colorIndex+3] = 127; // a
+                    }
+                }
+
+                return ret;
             }
         }
     }
